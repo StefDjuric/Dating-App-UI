68b2ffb7a52c7f90b7babe9a87dccfb80722cc3b
Stefan <djuric.stef4n@gmail.com>
2025-05-31 14:20:30 +0200

    Implement upload and delete photos

--- src/app/members/edit-member-details/edit-member-details.component.ts
+++ src/app/members/edit-member-details/edit-member-details.component.ts
@@ -10,10 +10,14 @@ import { MembersService } from '../../services/members.service';
 import { Member } from '../../models/Member';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CommonModule, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-edit-member-details',
-  imports: [FormsModule],
+  imports: [FormsModule, ButtonComponent, CommonModule, FileUploadModule],
   templateUrl: './edit-member-details.component.html',
   styleUrl: './edit-member-details.component.css',
 })
@@ -28,9 +32,38 @@ export class EditMemberDetailsComponent implements OnInit {
   private toastr = inject(ToastrService);
   member?: Member;
   activeTab = 'edit-info';
+  uploader?: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.baseUrl;
 
   ngOnInit(): void {
     this.getMemberByUsername();
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any) {
+    this.hasBaseDropzoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/upload-photo',
+      authToken: 'Bearer ' + this.accountService.currentUser()?.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024,
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      const photo = JSON.parse(response);
+      this.member?.photos.push(photo);
+    };
   }
 
   setActiveTab(tab: string) {
@@ -52,6 +85,29 @@ export class EditMemberDetailsComponent implements OnInit {
         this.toastr.success('User updated successfully.');
         this.editForm?.reset(this.member);
       },
+    });
+  }
+
+  setMainPhoto(photoId: number) {
+    this.memberService.setMainPhoto(photoId).subscribe({
+      next: (_) => {
+        this.toastr.success('Successfully set main photo. ');
+      },
+      error: (err) => this.toastr.error(err),
+    });
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: (_) => {
+        const updatedMember = { ...this.member };
+        updatedMember.photos = updatedMember.photos?.filter(
+          (p) => p.id !== photoId
+        );
+
+        this.toastr.success('Successfully deleted photo.');
+      },
+      error: (err) => this.toastr.error(err),
     });
   }
 }
